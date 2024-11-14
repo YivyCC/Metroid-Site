dee3d892eb3c17c7dc9fc97eb873eebb5da200cb
Ricardo Castro <ricardo20.castro00@gmail.com>
2024-11-13 23:18:00 -0800

    Updated the navbar for low-res versions

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const navBarBtn = document.querySelector('.header__navbar-btn');
+const navBarBtn = document.querySelector(".header__navbar-btn");
 const navBarList = document.querySelector(".navbar__nav");
 const navBar = document.querySelector(".navbar");
 
 navBarBtn.addEventListener("click", () =>{
-  navBarList.classList.add("appear");
-  navBar.classList.add("appear");
+  navBar.classList.toggle("appear");
+  navBarBtn.classList.toggle("nav-btn-toggle");
 });
