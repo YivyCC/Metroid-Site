df447c6e0549a37cbcd6af016e86095deb718e0b
Ricardo Castro <ricardo20.castro00@gmail.com>
2024-11-15 14:56:19 -0800

    Added sticky and transparency effects to the header as well as a bit of the main raw content

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const navBarBtn = document.querySelector(".header__navbar-btn");
 const navBarList = document.querySelector(".navbar__nav");
 const navBar = document.querySelector(".navbar");
+const header = document.querySelector(".header");
 
+//Nav-bar trigger btn
 navBarBtn.addEventListener("click", () =>{
   navBar.classList.toggle("appear");
   navBarBtn.classList.toggle("nav-btn-toggle");
+});
+
+//Header background color transparency 'animation'
+window.addEventListener('scroll', () =>{
+  const scrollPosition = window.scrollY;
+  const transparency = Math.max(0, 1 - scrollPosition / 150); 
+  header.style.background = `rgb(34, 34, 34, ${transparency})`;
 });
